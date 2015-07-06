f0a9494aed3c3ce786a5737fa7d5cdb6a234b3e8
unknown <pkuznetsov@kusnetov-t5400.corp.es.com>
2015-07-06 13:55:11 -0600

    Initial commit to the angularjs branch. Pretty much eliminated all static files and replaced them with dynamic angularjs handlers.

--- js/application.js
+++ js/application.js
@@ -1,4 +1,62 @@
-window.PhilKuz = Em.Application.create({
-	rootElement: $('#dolphin'), 
-	ready: function(){}
+var siteApp = angular.module('personalSite', ['ngRoute']);
+siteApp.value('title', "Phillip Kuznetsov");
+siteApp.config(function($routeProvider){
+	$routeProvider.
+		when('/', {
+			templateUrl: 'partials/index.part.html'
+		}).when('/about', {
+			templateUrl: 'partials/about.part.html'
+		}).when('/projects', {
+			templateUrl: 'partials/projectbrowser.part.html'
+		}).when('/projects/:link', {
+			templateUrl: 'partials/project.part.html'
+		}).otherwise({
+			redirectTo: '/'
+		});
 });
+siteApp.filter('projectURL', function(){
+	return function(string){
+		return "#/projects/"+string;
+	};
+});
+siteApp.factory('Projects', function($http){
+	function getData(callback){
+		$http({
+			method: 'GET', 
+			url:'api/projects.json', 
+			cache: true}).success(callback);
+	}
+	return {
+		//returns all of the posts
+		getProjects: getData, 
+		//finds the post that matches the link
+		find: function(link, callback){
+			getData(function(data){
+				var post = data.filter(function(entry){
+					return entry.link === link;
+				})[0];
+				callback(post);
+			});
+		}
+	};
+});
+//The controller for the entire webpage.
+siteApp.controller('SuperCtrl', ['$scope', 'title', function($scope, title){
+	$scope.title = title;
+	$scope.homeLink = "/";
+	$scope.links = [
+		{"string" : "Home", 		"url" : $scope.homeLink		},
+		{"string" : "About", 		"url" :"/#/about"	}, 
+		{"string" : "Projects", 	"url" :"/#/projects"}];
+}]);
+siteApp.controller('ProjectCtrl', ['$scope', 'Projects', function($scope, Projects){
+	Projects.getProjects(function(data){
+		$scope.projects = data;
+	});
+}]);
+siteApp.controller('SngProjectCtrl', ['$scope', '$routeParams', 'Projects', function($scope, $routeParams, Projects){
+	var link = $routeParams.link;
+	Projects.find(link, function(data){
+		$scope.project = data;
+	})
+}]);
--- philrkuz.py
+++ philrkuz.py
@@ -5,13 +5,8 @@ import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-    	user = users.get_current_user()
-    	if user:
-        	self.response.headers['Content-Type'] = 'text/plain'
-        	self.response.write("Hello, "+user.nickname())
-        else:
-        	self.redirect(users.create_login_url(self.request.uri))
-
+    	self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write("404 Page not found")
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/404', MainPage),
 ], debug=True)
